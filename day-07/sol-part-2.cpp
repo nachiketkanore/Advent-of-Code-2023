0f8dd44f9ecc99c50c1329da74127ca5fe45e48c
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-12-08 01:32:00 +0530

    AOC 2023 day 7 solutions - both parts

---
+++ day-07/sol-part-2.cpp
@@ -0,0 +1,139 @@
+/**
+ *    Author: Nachiket Kanore
+ *    Created: Friday 08 December 2023 12:49:11 AM IST
+ **/
+#include "bits/stdc++.h"
+using namespace std;
+
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define see(...) ;
+#endif
+
+#define int int64_t
+#define sz(x) (int)(x.size())
+#define ALL(x) (x).begin(), (x).end()
+#define F0R(i, R) for (int i = (0); i < (R); ++i)
+#define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
+
+vector<char> cards = { 'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J' };
+vector<vector<int>> types = {
+	{ 5 },
+	{ 1, 4 },
+	{ 2, 3 },
+	{ 1, 1, 3 },
+	{ 1, 2, 2 },
+	{ 1, 1, 1, 2 },
+	{ 1, 1, 1, 1, 1 },
+};
+vector<pair<string, int>> A;
+
+void input() {
+	string hand;
+	int bid_value;
+	while (cin >> hand >> bid_value)
+		A.push_back(make_pair(hand, bid_value));
+}
+
+int card_strength(char ch) {
+	F0R(i, sz(cards)) {
+		if (cards[i] == ch) {
+			return sz(cards) - i;
+		}
+	}
+	assert(false);
+	return -1;
+}
+
+int apply_old_logic(string S) {
+	map<char, int> freq;
+	for (char ch : S) {
+		freq[ch]++;
+	}
+	assert(freq.count('J') == 0);
+	vector<int> F;
+	for (auto [k, v] : freq) {
+		F.push_back(v);
+	}
+	sort(ALL(F));
+	F0R(type_id, sz(types)) {
+		if (F == types[type_id]) {
+			return type_id;
+		}
+	}
+
+	see("failed for ", S, F);
+	assert(false);
+	return -1;
+}
+
+int apply_new_logic(string S) {
+	assert(count(ALL(S), 'J') > 0);
+	int best_type = 10;
+	for (char ch : cards) {
+		if (ch != 'J') {
+			string T = S;
+			for (char& x : T)
+				if (x == 'J')
+					x = ch;
+			int type = apply_old_logic(T);
+			best_type = min(best_type, type);
+		}
+	}
+	assert(best_type != 10);
+	return best_type;
+}
+
+int get_type(string S) {
+	map<char, int> freq;
+	for (char ch : S) {
+		freq[ch]++;
+	}
+	if (freq.count('J') > 0) {
+		return apply_new_logic(S);
+	} else {
+		return apply_old_logic(S);
+	}
+}
+
+bool better(const string& one, const string& two) {
+	// one					two
+	// lower rank           higher rank
+	int type_one = get_type(one);
+	int type_two = get_type(two);
+
+	if (type_one < type_two) {
+		return true;
+	} else if (type_one > type_two) {
+		return false;
+	}
+	F0R(i, sz(one)) {
+		int c_one = card_strength(one[i]);
+		int c_two = card_strength(two[i]);
+		if (c_one > c_two) {
+			return true;
+		} else if (c_one < c_two) {
+			return false;
+		}
+	}
+	// assert(false);
+	return false;
+}
+
+int32_t main() {
+
+	input();
+	sort(A.begin(), A.end(), [&](pair<string, int> one, pair<string, int> two) {
+		return better(one.first, two.first);
+	});
+
+	reverse(ALL(A));
+	int ans = 0;
+	F0R(i, sz(A)) {
+		ans += (i + 1) * A[i].second;
+	}
+	cout << ans;
+
+	return 0;
+}
