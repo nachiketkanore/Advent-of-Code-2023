1d5cc3882074f5a3b6571b34b504a3593f250794
Nachiket Kanore <nachiket.kanore@gmail.com>
2023-12-18 14:42:51 +0530

    AOC 2023 day 18 solutions - part two incorrect approach

--- day-18/sol.cpp
+++ day-18/sol.cpp
@@ -17,80 +17,98 @@ using namespace std;
 #define F0R(i, R) for (int i = (0); i < (R); ++i)
 #define FOR(i, L, R) for (int i = (L); i <= (R); ++i)
 
-const int MAX = 2000;
-int N, M;
-bool vis[MAX][MAX];
+enum direction { right = 0, down = 1, left = 2, up = 3 };
+vector<array<int, 4>> vlines;
+int ci = 0, cj = 0000;
+int min_row = ci, max_row = ci;
+int border_length = 0;
+
+int get_dir(char ch) {
+	if (ch == 'R')
+		return 0;
+	if (ch == 'D')
+		return 1;
+	if (ch == 'L')
+		return 2;
+	if (ch == 'U')
+		return 3;
+	return -1;
+}
 
 void input() {
 	string line;
-	int ci = 1000, cj = 1000;
-	vis[ci][cj] = true;
+
+	// see(ci, cj);
+
 	while (getline(cin, line)) {
-		char dir = line[0];
-		int move = 0;
+		// int dist = stol(line.substr(sz(line) - 7, 5), nullptr, 16);
+		// int dir = stoi(line.substr(sz(line) - 2, 1));
+
+		int dist = 0;
 		if (isdigit(line[2]) && isdigit(line[3])) {
-			move = (line[2] - '0') * 10 + (line[3] - '0');
+			dist = (line[2] - '0') * 10 + (line[3] - '0');
 		} else if (isdigit(line[2])) {
-			move = line[2] - '0';
-		} else
-			assert(false);
+			dist = line[2] - '0';
+		}
+		int dir = get_dir(line[0]);
+		direction move = static_cast<direction>(dir);
+		border_length += dist;
 
-		if (dir == 'R') {
-			FOR(j, cj, cj + move) {
-				vis[ci][j] = true;
-			}
-			cj += move;
-		} else if (dir == 'L') {
-			FOR(j, cj - move, cj) {
-				vis[ci][j] = true;
-			}
-			cj -= move;
-		} else if (dir == 'U') {
-			FOR(i, ci - move, ci) {
-				vis[i][cj] = true;
-			}
-			ci -= move;
-		} else if (dir == 'D') {
-			FOR(i, ci, ci + move) {
-				vis[i][cj] = true;
-			}
-			ci += move;
-		} else
-			assert(false);
+		if (move == direction::left) {
+			// hlines.push_back({ ci, cj - dist, ci, cj });
+			cj -= dist;
+		}
+		if (move == direction::right) {
+			// hlines.push_back({ ci, cj, ci, cj + dist });
+			cj += dist;
+		}
+		if (move == direction::up) {
+			vlines.push_back({ ci - dist, cj, ci, cj });
+			ci -= dist;
+		}
+		if (move == direction::down) {
+			vlines.push_back({ ci, cj, ci + dist, cj });
+			ci += dist;
+		}
+		min_row = min(min_row, ci);
+		max_row = max(max_row, ci);
 	}
+
+	see(ci, cj);
+	see(min_row, max_row);
 }
-
-const int dx[] = { 0, 0, -1, 1 };
-const int dy[] = { -1, 1, 0, 0 };
-
-int vis_cnt = 0;
-void dfs(int i, int j) {
-	if (i < 0 || i >= MAX || j < 0 || j >= MAX)
-		return;
-	if (vis[i][j])
-		return;
-	vis_cnt++;
-	vis[i][j] = true;
-
-	F0R(id, 4) {
-		dfs(i + dx[id], j + dy[id]);
-	}
-}
-
 int32_t main() {
 	// freopen("test", "r", stdin);
+	freopen("in1", "r", stdin);
 	input();
+	int ans = 0;
 
-	vector<int> counts;
-	int vis_loop = 0;
-	F0R(i, MAX) F0R(j, MAX) vis_loop += vis[i][j];
-	F0R(i, MAX) F0R(j, MAX) if (!vis[i][j]) {
-		vis_cnt = 0;
-		dfs(i, j);
-		counts.push_back(vis_cnt);
+	FOR(row, min_row, max_row) {
+		// get sum of lengths of horizontal lines passing through this row
+
+		vector<int> verticals;
+		for (auto [r1, c1, r2, c2] : vlines) {
+			assert(c1 == c2);
+			assert(r1 < r2);
+			if (r1 <= row && row <= r2) {
+				verticals.push_back(c1);
+			}
+		}
+		sort(ALL(verticals));
+		int row_contrib = 0;
+
+		for (int i = 0; i + 1 < sz(verticals); i += 2) {
+			int c1 = verticals[i], c2 = verticals[i + 1];
+			row_contrib += c2 - c1 - 1;
+		}
+		see(row, row_contrib);
+		ans += row_contrib;
 	}
-	sort(ALL(counts));
-	cout << "answer = " << (vis_loop + counts[0]) << endl;
+
+	see(border_length);
+	see(ans);
+	ans += border_length;
+	see(ans);
 
 	return 0;
 }
